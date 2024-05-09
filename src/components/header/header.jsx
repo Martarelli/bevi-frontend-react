e0e78c78393ed97fb7be48de2fe24187238de129
Martarelli <renanmartarelli@gmail.com>
2024-05-09 14:47:08 -0300

    feat: show name on header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,49 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../../styles/header.css';
+import user from "../../assets/user-solid.svg"
+
 function Header() {
+    const [name, setName] = useState('');
+
     const navigate = useNavigate();
     function logout(){
         localStorage.clear();
         navigate('/login');
     }
 
-    const NAVBAR_STYLE = {
-        backgroundColor: 'rgba(34, 35, 68)',
+    async function getData() {
+        try {
+            const token = localStorage.getItem('access_token');
+            const response = await axios.create({
+              baseURL: 'http://34.71.240.100/api',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+              }
+            }).post('/auth/me');
+            console.log(response);
+            setName(response.data.name)
+            } catch (error) {
+                alert('Ocorreu um erro ao processar sua requisição...\n' + error.message);
+                console.log('Error fetching data: ' + error.message);
+                console.log(error);
+            }
     }
 
+    useEffect(()=>{
+        getData();
+      },[])
+
   return (
-    <nav className="navbar navbar-dark navbar-expand-lg w-100 p-3" style={NAVBAR_STYLE}>
+    <nav className="navbar navbar-dark navbar-expand-lg w-100 p-3" id='navbar'>
         <div className="container-fluid w-100 d-flex align-items-center justify-content-between">
             <div>
-                <a className="navbar-brand" role='button' href="#">Teste Bevi</a>
+                <a className="navbar-brand d-flex align-items-center" role='button' href="#">
+                    <img className="user__icon px-2 " src={user} alt="Login" />
+                    {name}
+                </a>
             </div>
             <div className="" >
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -25,11 +54,10 @@ function Header() {
                         <a className="nav-link " role='button' onClick={logout}>Logout</a>
                     </li>
                 </ul>
-
             </div>
         </div>
     </nav>
   )
 }
 
-export default Header
+export default Header;
