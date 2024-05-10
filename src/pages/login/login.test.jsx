e900e4fc91edaafdb26b8d6a03150abf8e3d6f21
martarelli-sim <renanmartarelli@gmail.com>
2024-05-09 21:42:33 -0300

    setup: jest working

---
+++ src/pages/login/login.test.jsx
@@ -0,0 +1,26 @@
+import Login from './login';
+import { render, screen } from '@testing-library/react'
+
+import { BrowserRouter } from "react-router-dom";
+
+describe('Login', () => {
+
+    it('should be render', () => {
+        render( <BrowserRouter>
+            <Login/>
+        </BrowserRouter>
+        );
+    });
+
+    it('should title be welcome', () => { 
+        render( <BrowserRouter>
+                    <Login/>
+                </BrowserRouter>
+        );
+
+        const title = screen.getByText('Seja Bem-Vindo');
+        expect(title).toBeInTheDocument();
+    });
+
+
+ })
